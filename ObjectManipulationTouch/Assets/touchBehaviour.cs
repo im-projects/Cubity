70bd92c505de581de64592ad7a09bb06a5e01c79
Babz <Babz@Babz-Laptop.(none)>
2013-12-18 00:56:04 +0100

    move obj funzt; todo offset skalieren; todo rotate u scale

--- ObjectManipulationTouch/Assets/touchBehaviour.cs
+++ ObjectManipulationTouch/Assets/touchBehaviour.cs
@@ -2,14 +2,8 @@
 using System.Collections;
 
 public class touchBehaviour : MonoBehaviour {
-	private Vector3 screenPoint;
-	private Vector3 firstPosition;
-	private Vector3 curScreenPoint;
-	private Vector3 curPosition;
-	//private Vector3 offset;
 
 	private GameObject selectedObj = null;
-	//private Vector3 positionSelectObj;
 
 	// Use this for initialization
 	void Start () {
@@ -26,6 +20,7 @@ public class touchBehaviour : MonoBehaviour {
 					//ray intersects any collider
 					if (Physics.Raycast (ray, out hit)) {
 						//ray hits rigidbody
+						//rigidbody should be used when obj is moving
 						if (hit.rigidbody != null) {
 							print ("box selected");
 							selectedObj = hit.rigidbody.gameObject;
@@ -43,7 +38,6 @@ public class touchBehaviour : MonoBehaviour {
 			} else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled) {
 				releaseSelection();
 				print ("Box Released!");
-				selectedObj = null;
 			} else {
 				print ("unknown touchphase");
 			}
@@ -52,66 +46,36 @@ public class touchBehaviour : MonoBehaviour {
 
 	public void initSelection() {
 		print ("selection init");
-		screenPoint = Camera.main.WorldToScreenPoint(selectedObj.transform.position);
-		firstPosition = selectedObj.transform.position;
 		selectedObj.renderer.material.color = Color.green;
 		print ("select color chosen");
 	}
 
 	public void initMove(Touch touch) {
-		if(selectedObj.tag == "moveX") {
+		if (selectedObj != null) {
+			if (selectedObj.tag == "moveX") {
+					float deltaPos = touch.deltaPosition.x / 10;
+					selectedObj.transform.Translate (Vector3.right * deltaPos, Space.Self);
 
-			curScreenPoint = new Vector3(touch.position.x, touch.position.y, screenPoint.z);
-			curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
-			float xOffset = curPosition.x - firstPosition.x;
-			print ("XOffset " + xOffset);
-			Vector3 newPos = new Vector3(selectedObj.transform.position.x + xOffset, selectedObj.transform.position.y, selectedObj.transform.position.z);
-			selectedObj.transform.position = newPos;
-			firstPosition.x += xOffset;
+			} else if (selectedObj.tag == "moveY") {
+					float deltaPos = touch.deltaPosition.y / 10;
+					selectedObj.transform.Translate (Vector3.up * deltaPos, Space.Self);
 
-		} else if(selectedObj.tag == "moveY") {
-			curScreenPoint = new Vector3(touch.position.x, touch.position.y, screenPoint.z);
-			curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
-			float yOffset = curPosition.y - firstPosition.y;
-			print ("YOffset " + yOffset);
-			Vector3 newPos = new Vector3(selectedObj.transform.position.x, selectedObj.transform.position.y + yOffset, selectedObj.transform.position.z);
-			selectedObj.transform.position = newPos;
-			firstPosition.y += yOffset;
-			
-		} else if(selectedObj.tag == "moveZ") {
-			curScreenPoint = new Vector3(touch.position.x, touch.position.y, screenPoint.z);
-			curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
-			float zOffset = curPosition.z - firstPosition.z;
-			print ("ZOffset " + zOffset);
-			Vector3 newPos = new Vector3(selectedObj.transform.position.x, selectedObj.transform.position.y, selectedObj.transform.position.z + zOffset);
-			selectedObj.transform.position = newPos;
-			firstPosition.z += zOffset;
-		}
+			} else if (selectedObj.tag == "moveZ") {
 
-	}
-	/*
-	public void translate(float offset, string tag) {
-		//if(isSelected){
-			Vector3 newPosition = transform.position;
-			if(tag == "moveX") {
-				newPosition.x += offset;
-			} else if(tag == "moveY") {
-				newPosition.y += offset;
-			} else if(tag == "moveZ") {
-				newPosition.z += offset;
+					float deltaPos = touch.deltaPosition.y / 10;
+					selectedObj.transform.Translate (Vector3.forward * deltaPos, Space.Self);
+			} else {
+					print ("obj has no tag");
 			}
-			selectedObj.transform.position = newPosition;
-		//}
+		}
 	}
-	*/
 
 	public void releaseSelection() {
 		print ("release selection");
-		//TODO ev fehlerquelle: reset screenPoint + firstPos
-		//screenPoint = null;
-		//firstPosition = null;
-		selectedObj.renderer.material.color = Color.red;
-		selectedObj = null;
+		if (selectedObj != null) {
+			selectedObj.renderer.material.color = Color.red;
+			selectedObj = null;
+		}
 	}
 
 }
