c1d90f05eaf3f3b3861e3b6eb549be4c383bc298
Babz <Babz@Babz-Laptop.(none)>
2013-12-17 15:50:49 +0100

    refactored; select ok, move too fast

--- ObjectManipulationTouch/Assets/touchBehaviour.cs
+++ ObjectManipulationTouch/Assets/touchBehaviour.cs
@@ -7,13 +7,10 @@ public class touchBehaviour : MonoBehaviour {
 	private Vector3 curScreenPoint;
 	private Vector3 curPosition;
 	//private Vector3 offset;
-	private bool moveX = false;
-	private bool moveY = false;
-	private bool moveZ = false;
 
 	private GameObject selectedObj = null;
-	private Vector3 positionSelectObj;
-	
+	//private Vector3 positionSelectObj;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -23,86 +20,70 @@ public class touchBehaviour : MonoBehaviour {
 	void Update () {
 		foreach (Touch touch in Input.touches) {
 			if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Stationary) {
-				print ("box selected");
+
 				Ray ray = Camera.main.ScreenPointToRay (touch.position);
 				RaycastHit hit;
+				//ray intersects any collider
 				if (Physics.Raycast (ray, out hit)) {
+					//ray hits rigidbody
 					if (hit.rigidbody != null) {
+						print ("box selected");
 						selectedObj = hit.rigidbody.gameObject;
-						
-						if (selectedObj.tag == "moveX") {
-							select (moveX);
-							screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-							firstPosition = gameObject.transform.position;
-							//offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
-						
-						} else if (selectedObj.tag == "moveY") {
-							select (moveY);
-							screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-							firstPosition = gameObject.transform.position;
-						
-						} else if (selectedObj.tag == "moveZ") {
-							select (moveZ);
-							screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-							firstPosition = gameObject.transform.position;
-						
-						} else {
-							print("obj has no tag!");
-						}
+						initSelection();
 					}
-				} else {
-					//TODO: impl movement of player
-				}
+					else {
+						//TODO: impl movement of player
+					}
+				} 
 				
-			}
-			if (touch.phase == TouchPhase.Moved) {
-				if(selectedObj.tag == "moveX") {
-					if(moveX) {
-						curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-						curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
-						float xOffset = curPosition.x - firstPosition.x;
-						print ("XOffset " + xOffset);
-						translate(xOffset, selectedObj.tag);
-						firstPosition.x += xOffset;
-					}
-				
-				} else if(selectedObj.tag == "moveY") {
-					if(moveY) {
-						curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-						curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
-						float yOffset = curPosition.y - firstPosition.y;
-						print ("YOffset " + yOffset);
-						translate(yOffset, selectedObj.tag);
-						firstPosition.y += yOffset;
-					}
-				
-				} else if(selectedObj.tag == "moveZ") {
-					if(moveZ) {
-						curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-						curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
-						float zOffset = curPosition.z - firstPosition.z;
-						print ("ZOffset " + zOffset);
-						translate(zOffset, selectedObj.tag);
-						firstPosition.z += zOffset;
-					}
-				}
+			} else if (touch.phase == TouchPhase.Moved) {
+				initMove();
 				print ("box moved");
-			}
-			if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled) {
-				if(selectedObj.tag == "moveX") {
-					unselect(moveX);
-				} else if(selectedObj.tag == "moveY") {
-					unselect(moveY);
-				} else if(selectedObj.tag == "moveZ") {
-					unselect(moveZ);
-				} else {
-					print ("no box was selected");
-				}
+			} else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled) {
+				releaseSelection();
 				print ("Box Released!");
 				selectedObj = null;
+			} else {
+				print ("unknown touchphase");
 			}
 		} //end foreach touch()
 	} //end Update()
+
+	public void initSelection() {
+		print ("selection init");
+		screenPoint = Camera.main.WorldToScreenPoint(selectedObj.transform.position);
+		firstPosition = selectedObj.transform.position;
+		selectedObj.renderer.material.color = Color.green;
+		print ("select color chosen");
+	}
+
+	public void initMove() {
+		if(selectedObj.tag == "moveX") {
+			curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+			curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+			float xOffset = curPosition.x - firstPosition.x;
+			print ("XOffset " + xOffset);
+			translate(xOffset, selectedObj.tag);
+			firstPosition.x += xOffset;
+			
+		} else if(selectedObj.tag == "moveY") {
+			curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+			curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+			float yOffset = curPosition.y - firstPosition.y;
+			print ("YOffset " + yOffset);
+			translate(yOffset, selectedObj.tag);
+			firstPosition.y += yOffset;
+			
+		} else if(selectedObj.tag == "moveZ") {
+			curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+			curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+			float zOffset = curPosition.z - firstPosition.z;
+			print ("ZOffset " + zOffset);
+			translate(zOffset, selectedObj.tag);
+			firstPosition.z += zOffset;
+		}
+
+	}
 
 	public void translate(float offset, string tag) {
 		//if(isSelected){
@@ -114,22 +95,17 @@ public class touchBehaviour : MonoBehaviour {
 			} else if(tag == "moveZ") {
 				newPosition.z += offset;
 			}
-			transform.position = newPosition;
+			selectedObj.transform.position = newPosition;
 		//}
 	}
 
-	public void select(bool cube) {
-		selectedObj.renderer.material.color = Color.green;
-		//selectedObj.renderer.material = Resources.Load("SelectMaterial", typeof(Material)) as Material;
-		print ("select color chosen");
-		cube = true;
-	}
-
-	public void unselect(bool cube) {
+	public void releaseSelection() {
+		print ("release selection");
+		//TODO ev fehlerquelle: reset screenPoint + firstPos
+		//screenPoint = null;
+		//firstPosition = null;
 		selectedObj.renderer.material.color = Color.red;
-		//selectedObj.renderer.material = Resources.Load("UnselectMaterial", typeof(Material)) as Material;
-		print ("unselect color chosen");
-		cube = false;
+		selectedObj = null;
 	}
 
 }
