6cde1a472b3b1495c58135c916c7761ecab9bb85
Andreas <s1301629009@students.fh-hagenberg.at>
2014-01-13 12:34:50 +0100

    adapt new cube manipulation scripts. extract input code to single InputManager script.

---
+++ Cubity Level Editor/Assets/Scripts/CubeManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeManager : MonoBehaviour {
+	
+	public ECubeType cubeType = ECubeType.NONE;
+	public Material materialNormal;
+	public Material materialSelected;
+
+	private bool isSelected = false;
+	
+	public enum ECubeType
+	{
+		NONE,
+		TRANSLATE_X,
+		TRANSLATE_Y,
+		TRANSLATE_Z,
+		ROTATE_X,
+		ROTATE_Y,
+		ROTATE_Z,
+		SCALE_X,
+		SCALE_Y,
+		SCALE_Z
+	}
+
+	void Start()
+	{
+		SetMaterial();
+	}
+
+	private void SetMaterial ()
+	{
+		Transform[] children = GetComponentsInChildren<Transform> ();
+		foreach (Transform child in children) {
+			if (
+				child.gameObject.name.Equals ("plane_north") || 
+				child.gameObject.name.Equals ("plane_east") || 
+				child.gameObject.name.Equals ("plane_south") || 
+				child.gameObject.name.Equals ("plane_west")
+				) 
+			{
+				child.gameObject.renderer.material = (isSelected) ? materialSelected : materialNormal;
+			}
+		}
+	}
+
+	public void SetSelected(bool value)
+	{
+		isSelected = value;
+		SetMaterial();
+	}
+}
---
+++ Cubity Level Editor/Assets/Scripts/InputManager.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+	private GameObject selectedObj = null;	
+
+	// Update is called once per frame
+	void Update () {
+		foreach (Touch touch in Input.touches) {
+			if (touch.phase == TouchPhase.Began) {
+				Debug.Log (touch.phase);
+				if(selectedObj == null) {
+					Ray ray = Camera.main.ScreenPointToRay (touch.position);
+					RaycastHit hit;
+					//ray intersects any collider
+					if (Physics.Raycast (ray, out hit)) {
+						//ray hits rigidbody
+						//rigidbody should be used when obj is moving
+						if (hit.rigidbody != null) {
+							if(hit.rigidbody.gameObject.GetComponent<CubeManager>() != null)
+							{
+//								print ("box selected");
+								selectedObj = hit.rigidbody.gameObject;
+								InitSelection();
+							}
+						}
+						else {
+							//TODO: impl movement of player
+						}
+					}
+				} 
+				
+			} else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary) {
+				if(selectedObj != null) {
+					InitMove(touch);
+//					print ("box moved");
+				}
+				else {
+					//TODO: impl movement of player
+				}
+			} else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled) {
+				if(selectedObj != null) {
+					ZeroizeRigidbodyForce ();
+					ResetSelection ();
+	//				print ("Box Released!");
+				}
+			} else {
+				Debug.LogWarning ("unknown touchphase");
+			}
+		}
+	}
+
+	private void InitMove(Touch touch) {
+		float deltaPos = 0;
+
+		switch (selectedObj.GetComponent<CubeManager>().cubeType)
+		{
+		case CubeManager.ECubeType.TRANSLATE_X:
+			deltaPos = touch.deltaPosition.x / 80;
+			selectedObj.transform.Translate (Vector3.right * deltaPos, Space.Self);
+			break;
+		case CubeManager.ECubeType.TRANSLATE_Y:
+			deltaPos = touch.deltaPosition.y / 80;
+			selectedObj.transform.Translate (Vector3.up * deltaPos, Space.Self);
+			break;
+		case CubeManager.ECubeType.TRANSLATE_Z:
+			deltaPos = touch.deltaPosition.y / 80;
+			selectedObj.transform.Translate (Vector3.forward * deltaPos, Space.Self);
+			break;
+		case CubeManager.ECubeType.ROTATE_X:
+			deltaPos = touch.deltaPosition.y;
+			selectedObj.transform.Rotate (Vector3.right * deltaPos, Space.Self);
+			break;
+		case CubeManager.ECubeType.ROTATE_Y:
+			deltaPos = touch.deltaPosition.y;
+			selectedObj.transform.Rotate (Vector3.right * deltaPos, Space.Self);
+			break;
+		case CubeManager.ECubeType.ROTATE_Z:
+			deltaPos = touch.deltaPosition.y;
+			selectedObj.transform.Rotate (Vector3.right * deltaPos, Space.Self);
+			break;
+		case CubeManager.ECubeType.SCALE_X:
+			if(touch.deltaPosition.y > touch.deltaPosition.x) {
+				selectedObj.transform.localScale += new Vector3(0, 0.1F, 0);
+			} else {
+				selectedObj.transform.localScale += new Vector3(0.1F, 0, 0);
+			}
+			break;
+		case CubeManager.ECubeType.SCALE_Y:
+			if(touch.deltaPosition.y > touch.deltaPosition.x) {
+				selectedObj.transform.localScale += new Vector3(0, 0.1F, 0);
+			} else {
+				selectedObj.transform.localScale += new Vector3(0.1F, 0, 0);
+			}
+			break;
+		case CubeManager.ECubeType.SCALE_Z:
+			if(touch.deltaPosition.y > touch.deltaPosition.x) {
+				selectedObj.transform.localScale += new Vector3(0, 0.1F, 0);
+			} else {
+				selectedObj.transform.localScale += new Vector3(0.1F, 0, 0);
+			}
+			break;
+		case CubeManager.ECubeType.NONE:
+			Debug.LogWarning("Cube doesn't have ECubeType");
+			break;
+		default:
+			Debug.LogWarning("Cube doesn't have ECubeType");
+			break;
+		}
+	}
+	
+	private void InitSelection() {
+		Debug.Log ("InitSelection");
+		selectedObj.GetComponent<CubeManager>().SetSelected(true);
+	}
+	
+	private void ResetSelection ()
+	{
+		Debug.Log ("ResetSelection");
+		selectedObj.GetComponent<CubeManager>().SetSelected(false);
+		selectedObj = null;
+	}
+
+	private void ZeroizeRigidbodyForce ()
+	{
+		// sets rigidbody velocity to zero, this avoid the cube to move after a collision
+		selectedObj.rigidbody.velocity = Vector3.zero;
+		selectedObj.rigidbody.angularVelocity = Vector3.zero;
+	}
+}
