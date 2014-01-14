199076b0ac39642e75647ee27a08eaa3b5045b91
cassiss <sarah.troschl@yahoo.de>
2014-01-14 23:27:30 +0100

    added jump to character movement
    jump in move character cube script - needs to be implemented as touch

--- ObjectManipulationTouch/Assets/CameraCubeMovement.cs
+++ ObjectManipulationTouch/Assets/CameraCubeMovement.cs
@@ -5,9 +5,13 @@ public class CameraCubeMovement : MonoBehaviour {
 
 	public float cameraSensitivity = 90;
 	public float normalMoveSpeed = 10;
+	public float jumpForce = 10;
+	public float currentJump = 0;
 
 	private float rotationX = 0.0f;
 	private float rotationY = 0.0f;
+
+	private bool isGrounded = true;
 
 	// Use this for initialization
 	void Start () {
@@ -25,10 +29,37 @@ public class CameraCubeMovement : MonoBehaviour {
 			transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
 		}
 
+		//Jump On Space
+		if (Input.GetKeyDown (KeyCode.Space) && isGrounded) {
+			print ("Jump");
+			isGrounded = false;
+			currentJump = jumpForce;
+			//transform.Translate (Vector3.up * normalMoveSpeed * Time.deltaTime);
+		}
+
 		transform.Translate (Vector3.forward * normalMoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
 		transform.Translate (Vector3.right * normalMoveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime);
+
+		//Jump
+		if(currentJump > 6.5f) {
+			transform.Translate(Vector3.up * currentJump * Time.deltaTime);
+			currentJump -= 0.1f;
+			if(currentJump <= 6.5f) {
+				print ("Stop Jumping");
+			}
+		}
+
 		//transform.position += transform.forward * normalMoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
 		//transform.position += transform.right * normalMoveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
 	
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.name != "wall")
+		{
+			print ("Grounded");
+			isGrounded = true;
+		}
 	}
 }
