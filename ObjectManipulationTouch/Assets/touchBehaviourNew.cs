b00a88cbf131f2534bad8d78744546931e7011cc
cassiss <sarah.troschl@yahoo.de>
2014-01-15 21:06:57 +0100

    touch behaviour new -> umgeschriebenes moveX (nicht getestet!!!!)
    so in der art hätt ich gedacht müsst es gehn, kA ob des funzt :/ hab mal
    nur den code kopiert und auf touch umgeschriebn, die stellen wos
    reinghört müsstn aba so passn

--- ObjectManipulationTouch/Assets/touchBehaviourNew.cs
+++ ObjectManipulationTouch/Assets/touchBehaviourNew.cs
@@ -2,12 +2,20 @@
 using System.Collections;
 
 public class touchBehaviour : MonoBehaviour {
-
+	
 	private GameObject selectedObj = null;
-
+	
+	//single Touch Variables for Moving -> Variablen aus MouseMoveInput Skripten
+	private Vector3 TouchStartScreenPoint; //screenPoint
+	private Vector3 TouchCurScreenPoint; //curScreenPoint
+	private Vector3 CubeStartPosition; //firstPosition
+	private Vector3 CubeCurPosition; //curPosition
+	private Vector3 offset; // offset
+	
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
@@ -24,7 +32,14 @@ public class touchBehaviour : MonoBehaviour {
 						if (hit.rigidbody != null) {
 							print ("box selected");
 							selectedObj = hit.rigidbody.gameObject;
-
+							
+							//init single touch variables -> what is done in OnMouseDown
+							if (selectedObj.tag == "moveX") {
+								TouchStartScreenPoint = Camera.main.WorldToScreenPoint(selectedObj.transform.position);
+								CubeStartPosition = selectedObj.transform.position;
+								offset = CubeStartPosition - Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, TouchStartScreenPoint.z));
+							}
+							
 							initSelection();
 						}
 						else {
@@ -44,44 +59,52 @@ public class touchBehaviour : MonoBehaviour {
 			}
 		} //end foreach touch()
 	} //end Update()
-
+	
 	public void initSelection() {
 		print ("selection init");
 		selectedObj.renderer.material.color = Color.green;
 		print ("select color chosen");
 	}
-
+	
 	public void initMove(Touch touch) {
 		if (selectedObj != null) {
-			if (selectedObj.tag == "moveX") {
-				float deltaPos = touch.deltaPosition.x / 80;
-				selectedObj.transform.Translate (Vector3.right * deltaPos, Space.Self);
-
+			if (selectedObj.tag == "moveX") { //what is done in OnMouseDrag + funktion in boxtranslatescript die aufgerufen wird -> einfach code reingehaut
+				TouchCurScreenPoint = new Vector3(touch.position.x, touch.position.y, TouchStartScreenPoint.z);
+				CubeCurPosition = Camera.main.ScreenToWorldPoint(TouchCurScreenPoint) + offset;
+				float xOffset = CubeCurPosition.x - CubeStartPosition.x;
+				Vector3 newPosition = transform.position;
+				newPosition.x += xOffset;
+				transform.position = newPosition;
+				CubeStartPosition.x += xOffset;
+				
+				//float deltaPos = touch.deltaPosition.x / 80;
+				//selectedObj.transform.Translate (Vector3.right * deltaPos, Space.Self);
+				
 			} else if (selectedObj.tag == "moveY") {
 				float deltaPos = touch.deltaPosition.y / 80;
 				selectedObj.transform.Translate (Vector3.up * deltaPos, Space.Self);
-
+				
 			} else if (selectedObj.tag == "moveZ") {
 				float deltaPos = touch.deltaPosition.y / 80;
 				selectedObj.transform.Translate (Vector3.forward * deltaPos, Space.Self);
-
+				
 			} else if (selectedObj.tag == "rotate") {
 				float deltaPos = touch.deltaPosition.y;
 				selectedObj.transform.Rotate (Vector3.right * deltaPos, Space.Self);
-
+				
 			} else if (selectedObj.tag == "scale") {
 				if(touch.deltaPosition.y > touch.deltaPosition.x) {
 					selectedObj.transform.localScale += new Vector3(0, 0.1F, 0);
 				} else {
 					selectedObj.transform.localScale += new Vector3(0.1F, 0, 0);
 				}
-
+				
 			} else {
 				print ("obj has no tag");
 			}
 		}
 	}
-
+	
 	public void releaseSelection() {
 		print ("release selection");
 		if (selectedObj != null) {
@@ -89,5 +112,5 @@ public class touchBehaviour : MonoBehaviour {
 			selectedObj = null;
 		}
 	}
-
+	
 }
