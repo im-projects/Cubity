1fd62ca6df248bd59359863c488319a10a0f03da
Babz <Babz@Babz-Laptop.(none)>
2014-01-27 18:03:59 +0100

    null reference when using camera move scripts

--- ObjectManipulationTouch/Assets/multiTouchBehaviour.cs
+++ ObjectManipulationTouch/Assets/multiTouchBehaviour.cs
@@ -3,113 +3,161 @@ using System.Collections;
 
 public class multiTouchBehaviour : MonoBehaviour {
 
-	private GameObject selectedObj = null;
-	//private Touch t0 = null;
-	//private Touch t1 = null;
+	private MoveCameraAndroidScript moveCamera;
+	private RotatePlayerAndroidScript rotatePlayer;
+	private MovePlayerForwardBackwardAndroidScript movePlayer;
+	private MovePlayerByClickingAndroidScript movePlayerByClicking;
+
+	public static GameObject selectedObj = null;
+//	public static int touchCount = 0;
+//	public static int tapCount = 0;
+
 	private bool pinch = false;
-	private bool pinchUp = false;
-	private bool pinchDown = false;
+	private bool pinchApart = false;
+	private bool pinchContract = false;
 
 	// Use this for initialization
 	void Start () {
-	
+		//get all scripts needed
+		moveCamera = FindObjectOfType<MoveCameraAndroidScript>();
+		rotatePlayer = FindObjectOfType<RotatePlayerAndroidScript>();
+		movePlayer = FindObjectOfType<MovePlayerForwardBackwardAndroidScript>();
+		movePlayerByClicking = FindObjectOfType<MovePlayerByClickingAndroidScript>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		foreach (Touch touch in Input.touches) {
-			//singleTouch
-			if(Input.touchCount == 1) {
-				print("taps: " + touch.tapCount);
-				sendRay(touch, touch.tapCount);
+			int touchCount = Input.touchCount;
+			print ("no of fingers: " + touchCount);
+			int tapCount = touch.tapCount;
+			print("taps: " + tapCount);
+			
 
-				//singleTouchGesture: moveX, moveY, moveZ, rotate
+			//single touch, single click
+			if(touchCount == 1 && tapCount == 1) {
+				//cube action, singleTouchGesture: moveX, moveY, moveZ, rotate
 				if(selectedObj != null) {
 					print ("single touch gesture init");
-					singleTouchGesture(touch);
-				}
-			} //end singleTouch
+					singleTouchGesture(touch); //= Input.GetTouch(0)
+				} //end singleTouch
 
-			//multiTouchGesture: scale, jump
-			if(Input.touchCount == 2) {
-				print("no of touches: " + Input.touchCount);
+				else {
+					//camera move
+					if(touch.phase == TouchPhase.Moved) {
+						//TODO
+						rotatePlayer.rotatePlayer(touch.deltaPosition.x * Time.deltaTime); //left and right
+						moveCamera.rotateCamera(touch.deltaPosition.x * Time.deltaTime); //up and down
+					} 
+					//player move
+					else {
+						
+					}
+				}
+			} 
+
+			//single touch, double click
+			else if (touchCount == 1 && tapCount == 2) {
+				Rigidbody rb = sendRay(touch);
+				//raycast hit rigidbody
+				if(rb != null) {
+					//double click on selected object
+					if(rb.gameObject == selectedObj) {
+						releaseSelection();
+					}
+					//double click on cube
+					else {
+						releaseSelection();
+						selectedObj = rb.gameObject;
+						initSelection();
+					}
+				} //else: do nothing
+			}
+
+			//multi touch
+			else if (touchCount == 2) {
 				Touch t0 = Input.GetTouch(0);
 				Touch t1 = Input.GetTouch(1);
 
-				//no obj selected: character/camera action
-				if(selectedObj == null) {
-					//TODO implement
+				//cube action, multiTouchGesture: pinch (scale)
+				if(selectedObj != null && (t0.phase == TouchPhase.Moved || t1.phase == TouchPhase.Moved)) {
+					twoTouchGesture(t0, t1);
+				}
+
+				if(selectedObj != null && (t0.phase == TouchPhase.Ended || t1.phase == TouchPhase.Ended)) {
+					pinch = false;
+					pinchApart = false;
+					pinchContract = false;
+				}
+				/*
+				//player jumps or camera move
+				if(selectedObj == null && (t0.phase == TouchPhase.Moved || t1.phase == TouchPhase.Moved) {
 					//2 fingers parallel to make character jump
 					if(t0.deltaPosition.y > t0.deltaPosition.x && t1.deltaPosition.y > t1.deltaPosition.x) {
 						print("JUMP");
 					}
-
-					checkPinch(t0, t1);
-					//pinch for camera zoom
-					if(pinch) {
-						print("CAMERA ZOOM");
-					}
 				}
-
-				//obj action
-				if(selectedObj != null && (t0.phase == TouchPhase.Moved || t1.phase == TouchPhase.Moved)) {
-					//TODO desired behaviour?
-					print ("2 touch gesture init");
-					print ("pinch = " + pinch);
-					twoTouchGesture(t0, t1);
-				}
-
-				if (t0.phase == TouchPhase.Ended || t1.phase == TouchPhase.Ended) {
-					pinch = false;
-					pinchUp = false;
-					pinchDown = false;
-				}
+				*/
 			}
 
+			//more than 2 fingers on screen
+			else {
+				print ("ERROR: no action intended for " + touchCount + " fingers on screen");
+			}
 		} //end foreach touch()
 	} //end update()
 
-	public void sendRay(Touch touch, int tapCount) {
-		releaseSelection ();
+
+	//(0, 0) is lower left corner
+	//print ("pixel coord : x" + touch.position.x + " " + touch.position.y);
+	public static float movementOnXAxis(Touch t0) {
+		//movement on x axis
+		if (t0.deltaPosition.x > t0.deltaPosition.y) {
+			Vector2 prevPos = t0.position - t0.deltaPosition;
+			Vector2 currPos = t0.position;
+
+			//movement to left
+			if(prevPos.x > currPos.x) {
+				return -t0.deltaPosition.x;
+			} else {
+				return t0.deltaPosition.x;
+			}
+		}
+		return 0;
+	}
+
+	public static float movementOnYAxis(Touch t0) {
+		//movement on y axis
+		if (t0.deltaPosition.y > t0.deltaPosition.x) {
+			Vector2 prevPos = t0.position - t0.deltaPosition;
+			Vector2 currPos = t0.position;
+
+			//movement: down
+			if(prevPos.y > currPos.y) {
+				return -t0.deltaPosition.y;
+			} else {
+				return t0.deltaPosition.y;
+			}
+		}
+		return 0;
+	}
+
+	public Rigidbody sendRay(Touch touch) {
+		//releaseSelection ();
 		Ray ray = Camera.main.ScreenPointToRay (touch.position);
 		RaycastHit hit;
 		//ray intersects any collider
 		if (Physics.Raycast (ray, out hit)) {
 			//ray hits rigidbody
 			//rigidbody should be used when obj is moving
-			if (hit.rigidbody != null) {
-				print ("box selected");
-				if(tapCount == 2) {
-					selectedObj = hit.rigidbody.gameObject;
-					initSelection();
-				}
-			}
-			else {
-				if(selectedObj != null) {
-					selectedObj = null;
-				}
-				/*
-				 * doubleclick selects obj
-				 */
-				if(tapCount == 1) {
-					movePlayer();
-				}
-			}
+			return hit.rigidbody;
 		}
-	}
-
-	public void movePlayer() {
-		//TODO implement
-	}
-
-	public void jump() {
-		//TODO implement
+		return null;
 	}
 
 	public void initSelection() {
 		print ("selection init");
 		selectedObj.renderer.material.color = Color.green;
-		print ("select color chosen");
 	}
 
 	public void releaseSelection() {
@@ -129,7 +177,7 @@ public class multiTouchBehaviour : MonoBehaviour {
 
 		if (tag == "scale" && pinch == true) {
 			print("PINCH SCALE");
-			if(pinchUp) {
+			if(pinchApart) {
 				//scale up
 				selectedObj.transform.localScale += new Vector3 (0.1F, 0, 0);
 				/*
@@ -139,7 +187,7 @@ public class multiTouchBehaviour : MonoBehaviour {
 					selectedObj.transform.localScale += new Vector3 (0.1F, 0, 0);
 				}
 				*/
-			} else if(pinchDown) {
+			} else if(pinchContract) {
 				//scale down
 				selectedObj.transform.localScale += new Vector3 (-0.1F, 0, 0);
 				/*
@@ -164,21 +212,22 @@ public class multiTouchBehaviour : MonoBehaviour {
 	}
 
 	public void checkPinch(Touch t0, Touch t1) {
-		//TODO differentiate pinch together and pinch farthener
-		//TODO on touchPhase End -> release; pinch = false;
 		float prevDistance = Vector2.Distance (t0.position - t0.deltaPosition, t1.position - t1.deltaPosition);
 		float currDistance = Vector2.Distance (t0.position, t1.position);
 
 		if (prevDistance < currDistance) {
 			pinch = true;
-			pinchUp = true;
+			pinchApart = true;
+			pinchContract = false;
 			print ("fingers moving apart");
 		} else if (currDistance < prevDistance) {
 			pinch = true;
-			pinchDown = true;
+			pinchContract = true;
+			pinchApart = false;
 			print ("fingers moving together");
 		} else {
 			print ("no pinch");
+			pinch = false;
 		}
 	}
 	
@@ -207,39 +256,5 @@ public class multiTouchBehaviour : MonoBehaviour {
 			}
 		}
 	}
-	/*
-	Vector3 screenPoint;
-	Vector3 curScreenPoint;
-	Vector3 boxCenterPoint;
-	float oldDistance;
-	public float MaxScaleInPercent = 200.0f; //doppelt so groß
-	public float MinScaleInPercent = 50.0f; //halb so groß
-	Vector3 initialScale;
-	private float currentScale;
 	
-	private void doScale() {
-		//scale depends on drag direction -> from center bigger, to center smaller
-		//if drag goes over center it will scale according to which distance from the center is bigger
-		curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, boxCenterPoint.z);
-		float newDistance = (curScreenPoint - boxCenterPoint).magnitude;
-		float scaleDistance = newDistance - oldDistance;
-		float scaleFactor = 1;
-		scaleFactor += (scaleDistance/80);
-		
-		//boxScaleScript.scaleCube(scaleFactor);
-		Vector3 newScale = transform.localScale*scaleFactor;
-		currentScale *= scaleFactor;
-		//beschränkung skalierung hardcoded, später mit variablen
-		if(currentScale < (MinScaleInPercent/100)) {
-			newScale = initialScale*(MinScaleInPercent/100);
-			currentScale = (MinScaleInPercent/100);
-		}
-		if(currentScale > (MaxScaleInPercent/100)) {
-			newScale = initialScale*(MaxScaleInPercent/100);
-			currentScale = (MaxScaleInPercent/100);
-		}
-		transform.localScale = newScale;
-		oldDistance = newDistance;
-	}
-	*/
 }
